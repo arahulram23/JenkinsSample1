95488dc1f0f8889b9d8102b5dbde365f065cf2fb
arr2311 <142573791+arr2311@users.noreply.github.com>
2023-08-27 22:11:10 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello, World!");
       System.out.println("Tony Stark");
       System.out.println("I'm Ironman");
+        System.out.println("The GIT Sample project is successful");
     }
 }
