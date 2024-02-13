6b44582a5e2cc2140648a234c62897c0087396f7
arahulram23 <rahulram23@hotmail.com>
2024-02-13 18:47:38 +0530

    Local branch1

--- GIT.java
+++ GIT.java
@@ -1,11 +1,7 @@
 public class GIT {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-      System.out.println("Tony Stark");
-      System.out.println("I'm Ironman");
-        System.out.println("The GIT Sample project is successful");
-System.out.println("The GIT Sample project is successful");
-              System.out.println("I'm Ironman");
-        
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+
+
+  }
 }
